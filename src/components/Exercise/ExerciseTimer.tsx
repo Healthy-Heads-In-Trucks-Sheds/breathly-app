637a7d340a5ee8981452a465d149dc9f9f4ab3aa
Ray Chuan Tay <rtay@vmware.com>
2021-12-03 16:49:25 +0800

    Update rule name for react-hooks/exhaustive-deps

--- src/components/Exercise/ExerciseTimer.tsx
+++ src/components/Exercise/ExerciseTimer.tsx
@@ -44,7 +44,7 @@ export const ExerciseTimer: FC<Props> = ({ limit, onLimitReached }) => {
       onLimitReached();
     }
     // TODO: Check this
-    // eslint-disable-next-line react-app/react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [limit, elapsedTime]);
 
   const containerAnimatedStyle = {
