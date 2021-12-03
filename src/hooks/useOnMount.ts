b6bf1992e9215f662044cf281bd4b66cafef9a2a
Ray Chuan Tay <rtay@vmware.com>
2021-12-03 16:49:47 +0800

    Add eslint ignore for useOnMount()

--- src/hooks/useOnMount.ts
+++ src/hooks/useOnMount.ts
@@ -9,5 +9,6 @@ import { useEffect } from "react";
 type EffectCallback = () => void | (() => void);
 
 export function useOnMount(onMount: EffectCallback) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(onMount, []);
 }
